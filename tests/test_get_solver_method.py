bf645f8ddfa4055f530fb4452992c826934a44a6
DavidHurst <davidshrst@gmail.com>
2023-06-25 21:49:24 +0100

    Updated solver test

--- tests/test_get_solver_method.py
+++ tests/test_get_solver_method.py
@@ -5,7 +5,29 @@ import pytest
 from irfold import IRFoldBase, IRFoldVal1, IRFoldVal2, IRFoldCor2
 
 @pytest.mark.parametrize("irfold", [IRFoldBase, IRFoldVal1, IRFoldVal2, IRFoldCor2])
-def test_valid_output_type(data_dir, irfold, list_of_irs, rna_seq_15_bases_3_irs):
+def test_solver_generated_0_irs(data_dir, irfold, rna_seq_15_bases_0_irs):
+    seq = rna_seq_15_bases_0_irs[0]
+    seq_len = len(seq)
+
+    solver, variables = irfold.get_solver(
+        [],
+        seq_len,
+        seq,
+        data_dir,
+        "test_get_solver_seq",
+    )
+
+
+    assert solver is not None
+    assert variables is not None
+
+    assert isinstance(solver, CpModel)
+    assert isinstance(variables, list)
+
+    assert len(variables) == 0
+
+@pytest.mark.parametrize("irfold", [IRFoldBase, IRFoldVal1, IRFoldVal2, IRFoldCor2])
+def test_solver_generated_3_irs(data_dir, irfold, list_of_irs, rna_seq_15_bases_3_irs):
     seq = rna_seq_15_bases_3_irs[0]
     seq_len = len(seq)
 
