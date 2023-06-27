b90331dabe88e33eff2f5067aee9ce6008a9dcea
DavidHurst <davidshrst@gmail.com>
2023-06-27 22:29:15 +0100

    Added  test cases for disjoint and wholly nested IR tests

--- tests/test_irs_disjoint.py
+++ tests/test_irs_disjoint.py
@@ -6,3 +6,10 @@ def test_irs_disjoint(entirely_disjoint_ir_pair):
     ir_b = entirely_disjoint_ir_pair[1]
 
     assert ir_pair_disjoint(ir_a, ir_b) == True
+
+
+def test_irs_not_disjoint(wholly_nested_ir_pair):
+    ir_a = wholly_nested_ir_pair[0]
+    ir_b = wholly_nested_ir_pair[1]
+
+    assert ir_pair_disjoint(ir_a, ir_b) == False
--- tests/test_irs_wholly_nested.py
+++ tests/test_irs_wholly_nested.py
@@ -6,3 +6,10 @@ def test_irs_wholly_nested(wholly_nested_ir_pair):
     ir_b = wholly_nested_ir_pair[1]
 
     assert ir_pair_wholly_nested(ir_a, ir_b) == True
+
+
+def test_irs_not_wholly_nested(entirely_disjoint_ir_pair):
+    ir_a = entirely_disjoint_ir_pair[0]
+    ir_b = entirely_disjoint_ir_pair[1]
+
+    assert ir_pair_wholly_nested(ir_a, ir_b) == False
