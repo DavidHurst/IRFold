5db091ef71680e5670e075f7ad229f75ca444883
DavidHurst <davidshrst@gmail.com>
2023-06-28 17:56:55 +0100

    Tidied up experiment to highlight analysis of IR pairs from a single sequence

--- scripts/experiment_1_investigate_ir_pair_fe_additivity.py
+++ scripts/experiment_1_investigate_ir_pair_fe_additivity.py
@@ -10,27 +10,19 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parents[1]))
 
 from irfold import IRFoldBase
-from irfold.util import irs_to_dot_bracket, calc_free_energy, ir_pair_match_same_bases
+from irfold.util import (
+    irs_to_dot_bracket,
+    calc_free_energy,
+    ir_pair_match_same_bases,
+    ir_pair_forms_valid_loop,
+    ir_pair_wholly_nested,
+    ir_pair_disjoint,
+)
 
 DATA_DIR = str(Path(__file__).parent.parent / "data")
 
 
-def irs_wholly_nested(outer_ir, nested_ir):
-    # ToDo: Make this return true for nesting of A in B or B in A
-    return outer_ir[0][0] < nested_ir[0][0] and nested_ir[1][1] < outer_ir[1][0]
-
-
-def irs_disjoint(ir_a, ir_b):
-    # ir_a comes entirely before ir_b
-    ir_a_strictly_before_ir_b = ir_a[1][1] < ir_b[0][0]
-
-    # ir_b comes entirely before ir_a
-    ir_b_strictly_before_ir_a = ir_b[1][1] < ir_a[0][0]
-
-    return ir_a_strictly_before_ir_b or ir_b_strictly_before_ir_a
-
-
-def irs_fe_union_fe_sum(ir_a, ir_b, sequence_len, out_dir):
+def ir_pair_free_energy_calculation_variations(ir_a, ir_b, sequence_len, out_dir):
     # FE(IR_a) + FE(IR_b)
     ir_a_db_repr = irs_to_dot_bracket([ir_a], sequence_len)
     ir_b_db_repr = irs_to_dot_bracket([ir_b], sequence_len)
@@ -43,30 +35,7 @@ def irs_fe_union_fe_sum(ir_a, ir_b, sequence_len, out_dir):
     ir_a_b_db_repr = irs_to_dot_bracket([ir_a, ir_b], sequence_len)
     fe_union = round(calc_free_energy(ir_a_b_db_repr, seq, out_dir), 4)
 
-    return (ir_a_fe, ir_b_fe), fe_union, fe_sum
-
-
-def irs_form_valid_loop(ir_a, ir_b):
-    """This won't scale up"""
-
-    latest_left_string_base_idx = ir_a[0][1] if ir_a[0][1] > ir_b[0][1] else ir_b[0][1]
-    earliest_right_string_base_idx = (
-        ir_a[1][0] if ir_a[1][0] < ir_b[1][0] else ir_b[1][0]
-    )
-
-    # Case 1: Disjoint
-    if irs_disjoint(ir_a, ir_b):
-        return True
-
-    # Case 2: Invalid hairpin
-    # ToDo: Test for if the bases between being paired or not makes a difference
-    bases_inbetween_parens = (
-        earliest_right_string_base_idx - latest_left_string_base_idx - 1
-    )
-    if bases_inbetween_parens < 3:
-        return False
-
-    return True
+    return ir_a_fe, ir_b_fe, fe_union, fe_sum
 
 
 def get_all_ir_pairs_not_matching_same_bases_valid_gap_sz(all_irs):
@@ -84,10 +53,10 @@ def get_all_ir_pairs_not_matching_same_bases_valid_gap_sz(all_irs):
     ]
 
 
-def id_base_pairs(db_repr, irs):
+def identify_base_pairs(db_repr, irs):
     """Assigns the index value of IRs in the list provided to each base that
     IRs pair respectively"""
-    ids = ["A", "B", "C", "D", "E"]
+    ids = ["A", "B", "C", "D", "E", "F", "G", "H"]
     db_repr_id_assigned = list(db_repr)
 
     for i, ir in enumerate(irs):
@@ -101,32 +70,35 @@ def id_base_pairs(db_repr, irs):
     return "".join(db_repr_id_assigned)
 
 
-def eval_ir_pair_structure_and_mfe(ir_a, ir_b, seq_len, print_out=True):
-    (ir_a_fe, ir_b_fe), fe_union, fe_sum = irs_fe_union_fe_sum(
-        ir_a, ir_b, seq_len, DATA_DIR
-    )
+def eval_ir_pair_structure_and_mfe(pair_idx, ir_a, ir_b, seq_len, print_out):
+    (
+        ir_a_fe,
+        ir_b_fe,
+        pairs_fe_union,
+        pairs_fe_sum,
+    ) = ir_pair_free_energy_calculation_variations(ir_a, ir_b, seq_len, DATA_DIR)
 
-    additivity_assumption_held = fe_sum == fe_union
-    valid_loop_formed = irs_form_valid_loop(ir_a, ir_b)
+    additivity_assumption_held = pairs_fe_sum == pairs_fe_union
+    valid_loop_formed = ir_pair_forms_valid_loop(ir_a, ir_b)
 
     db_repr = irs_to_dot_bracket([ir_a, ir_b], seq_len)
-    db_repr_id_assigned = id_base_pairs(db_repr, [ir_a, ir_b])
+    db_repr_id_assigned = identify_base_pairs(db_repr, [ir_a, ir_b])
 
     if print_out:
         ir_a_fmt = f"[{str(ir_a[0][0]).zfill(2)}->{str(ir_a[0][1]).zfill(2)} {str(ir_a[1][0]).zfill(2)}->{str(ir_a[1][1]).zfill(2)}]"
         ir_b_fmt = f"[{str(ir_b[0][0]).zfill(2)}->{str(ir_b[0][1]).zfill(2)} {str(ir_b[1][0]).zfill(2)}->{str(ir_b[1][1]).zfill(2)}]"
 
         print_space = " " * 4
-        print(f"A = {ir_a_fmt}, B = {ir_b_fmt}")
-        print(print_space, f"A    : {irs_to_dot_bracket([ir_a], seq_len)}")
-        print(print_space, f"B    : {irs_to_dot_bracket([ir_b], seq_len)}")
-        print(print_space, f"A U B: {db_repr}")
-        print(print_space, f"A U B: {db_repr_id_assigned}")
+        print(f"Pair #{pair_idx}: A = {ir_a_fmt}, B = {ir_b_fmt}")
+        print(print_space, f"A DB    : {irs_to_dot_bracket([ir_a], seq_len)}")
+        print(print_space, f"B DB    : {irs_to_dot_bracket([ir_b], seq_len)}")
+        print(print_space, f"A U B DB: {db_repr}")
+        print(print_space, f"A U B DB: {db_repr_id_assigned}")
         print()
         print(print_space, f"FE(A)        : {ir_a_fe:.4f}")
         print(print_space, f"FE(B)        : {ir_b_fe:.4f}")
-        print(print_space, f"FE(A) + FE(B): {fe_sum:.4f}")
-        print(print_space, f"FE(A U B)    : {fe_union:.4f}")
+        print(print_space, f"FE(A) + FE(B): {pairs_fe_sum:.4f}")
+        print(print_space, f"FE(A U B)    : {pairs_fe_union:.4f}")
         print()
         print(print_space, f"Valid loop      : {valid_loop_formed}")
         print(print_space, f"Assumption holds: {additivity_assumption_held}")
@@ -134,130 +106,123 @@ def eval_ir_pair_structure_and_mfe(ir_a, ir_b, seq_len, print_out=True):
     return (
         additivity_assumption_held,
         valid_loop_formed,
-        ir_a,
-        ir_b,
         ir_a_fe,
         ir_b_fe,
-        fe_sum,
-        fe_union,
+        pairs_fe_sum,
+        pairs_fe_union,
     )
 
 
 if __name__ == "__main__":
+    """Evaluate the IR pairs found in a single sequence"""
+
     random.seed(223332)
     experiment_results = {
         "seq": [],
         "seq_len": [],
-        "num_valid_ir_gap_valid_base_pairing_pairs": [],
-        "ir_pair_index": [],
         "ir_a": [],
         "ir_b": [],
         "ir_a_fe": [],
         "ir_b_fe": [],
         "ir_pair_fe_sum": [],
         "ir_pair_fe_union": [],
-        "irs_wholly_nested": [],
-        "irs_partially_nested": [],
-        "irs_disjoint": [],
-        "additivity_assumption_held": [],
-        "irs_form_valid_loop": [],
+        "fe_additivity_assumption_held": [],
+        "ir_pair_forms_valid_loop": [],
+        "ir_pair_wholly_nested": [],
+        "ir_pair_partially_nested": [],
+        "ir_pair_disjoint": [],
     }
 
-    evaluated_pair_index = 0
-    for _ in range(2):
-        sequence_len = 30
-        seq = "".join(random.choice("ACGU") for _ in range(sequence_len))
+    sequence_len = 30
+    seq = "".join(random.choice("ACGU") for _ in range(sequence_len))
 
-        print(f"Seq. length: {sequence_len}")
-        print(f"Seq.       : {seq}")
+    print(f"Seq. length: {sequence_len}")
+    print(f"Seq.       : {seq}")
 
-        find_irs_kwargs = {
-            "sequence": seq,
-            "min_len": 2,
-            "max_len": sequence_len,
-            "max_gap": sequence_len - 1,
-            "mismatches": 0,
-            "out_dir": DATA_DIR,
-        }
-        found_irs = IRFoldBase.find_irs(**find_irs_kwargs)
-        n_irs = len(found_irs)
+    find_irs_kwargs = {
+        "sequence": seq,
+        "min_len": 2,
+        "max_len": sequence_len,
+        "max_gap": sequence_len - 1,
+        "mismatches": 0,
+        "out_dir": DATA_DIR,
+    }
+    found_irs = IRFoldBase.find_irs(**find_irs_kwargs)
+    n_irs = len(found_irs)
 
-        # Find all compatible IR pairs
-        print("Compatible IR Pairs".center(60, "="))
-        # ToDo: Also evaluate triples, quadruples etc.
+    # Find all compatible IR pairs
+    print("Compatible IR Pairs".center(60, "="))
 
-        separate_base_matching_ir_pairs = (
-            get_all_ir_pairs_not_matching_same_bases_valid_gap_sz(found_irs)
+    ir_pairs_not_matching_same_bases = (
+        get_all_ir_pairs_not_matching_same_bases_valid_gap_sz(found_irs)
+    )
+    wholly_nested_ir_pairs = []
+    disjoint_ir_pairs = []
+    partially_nested_ir_pairs = []
+
+    # Evaluate IR pairs and group pairs into three groups: wholly nested, partially nested or disjoint
+    for i, (ir_i, ir_j) in enumerate(ir_pairs_not_matching_same_bases):
+        (
+            assumption_held,
+            valid_loop_formed,
+            ir_a_fe,
+            ir_b_fe,
+            fe_sum,
+            fe_union,
+        ) = eval_ir_pair_structure_and_mfe(i, ir_i, ir_j, sequence_len, print_out=True)
+        experiment_results["seq"].append(seq)
+        experiment_results["seq_len"].append(sequence_len)
+        experiment_results["ir_a"].append(ir_i)
+        experiment_results["ir_b"].append(ir_j)
+        experiment_results["ir_a_fe"].append(ir_a_fe)
+        experiment_results["ir_b_fe"].append(ir_b_fe)
+        experiment_results["ir_pair_fe_sum"].append(fe_sum)
+        experiment_results["ir_pair_fe_union"].append(fe_union)
+        experiment_results["fe_additivity_assumption_held"].append(assumption_held)
+        experiment_results["ir_pair_forms_valid_loop"].append(valid_loop_formed)
+
+        pair_type_indicator_binary_repr = [
+            0,
+            0,
+            0,
+        ]  # Wholly nested, partially nested, disjoint
+        if ir_pair_wholly_nested(ir_i, ir_j):
+            pair_type_indicator_binary_repr[0] = 1
+            wholly_nested_ir_pairs.append((ir_i, ir_j))
+        if not ir_pair_wholly_nested(ir_i, ir_j) and not ir_pair_disjoint(ir_i, ir_j):
+            pair_type_indicator_binary_repr[1] = 1
+            partially_nested_ir_pairs.append((ir_i, ir_j))
+        if ir_pair_disjoint(ir_i, ir_j):
+            pair_type_indicator_binary_repr[2] = 1
+            disjoint_ir_pairs.append((ir_i, ir_j))
+
+        experiment_results["ir_pair_wholly_nested"].append(
+            pair_type_indicator_binary_repr[0]
+        )
+        experiment_results["ir_pair_partially_nested"].append(
+            pair_type_indicator_binary_repr[1]
+        )
+        experiment_results["ir_pair_disjoint"].append(
+            pair_type_indicator_binary_repr[2]
         )
 
-        # Classify IR pairs into three groups: wholly nested, partially nested or disjoint
-        wholly_nested_ir_pairs = [
-            pair
-            for pair in separate_base_matching_ir_pairs
-            if irs_wholly_nested(pair[0], pair[1])
-            or irs_wholly_nested(pair[0], pair[1])
-        ]
-        disjoint_ir_pairs = [
-            pair
-            for pair in separate_base_matching_ir_pairs
-            if irs_disjoint(pair[0], pair[1])
-        ]
-        partially_nested_ir_pairs = [
-            pair
-            for pair in separate_base_matching_ir_pairs
-            if pair not in wholly_nested_ir_pairs and pair not in disjoint_ir_pairs
-        ]
+    print("=" * 60)
+    print(f"IUPACpal Parameters:")
+    for kw, kwarg in find_irs_kwargs.items():
+        if kw.startswith("m"):
+            print(f"  - {kw} = {kwarg}")
+    print(f"Num. IRs found                    : {n_irs}")
+    print(f"Num. unique IR pairs              : {comb(n_irs, 2)}")
+    print(
+        f"Num. pairs not matching same bases: {len(ir_pairs_not_matching_same_bases)}"
+    )
+    print(f"Num. wholly nested pairs          : {len(wholly_nested_ir_pairs)}")
+    print(f"Num. partially nested pairs       : {len(partially_nested_ir_pairs)}")
+    print(f"Num. disjoint pairs               : {len(disjoint_ir_pairs)}")
 
-        print(f"IUPACpal Parameters:")
-        for kw, kwarg in find_irs_kwargs.items():
-            if kw.startswith("m"):
-                print(f"  - {kw} = {kwarg}")
-        print(f"IRs found             : {n_irs}")
-        print(f"Unique pairs          : {comb(n_irs, 2)}")
-        print(f"Not over-pairing bases: {len(separate_base_matching_ir_pairs)}")
-        print(f"Wholly nested         : {len(wholly_nested_ir_pairs)}")
-        print(f"Partially nested      : {len(partially_nested_ir_pairs)}")
-        print(f"Disjoint              : {len(disjoint_ir_pairs)}")
-
-        for pair_type_bin_str, pair_type, pair_list in zip(
-            [[1, 0, 0], [0, 1, 0], [0, 0, 1]],
-            ["Wholly Nested", "Partially Nested", "Disjoint"],
-            [wholly_nested_ir_pairs, partially_nested_ir_pairs, disjoint_ir_pairs],
-        ):
-            print(f"{pair_type} Pairs".center(60, "="))
-
-            for (ir_i, ir_j) in pair_list:
-                (
-                    assumption_held,
-                    valid_loop_formed,
-                    ir_a,
-                    ir_b,
-                    ir_a_fe,
-                    ir_b_fe,
-                    fe_sum,
-                    fe_union,
-                ) = eval_ir_pair_structure_and_mfe(
-                    ir_i, ir_j, sequence_len, print_out=True
-                )
-                experiment_results["seq"].append(seq)
-                experiment_results["seq_len"].append(sequence_len)
-                experiment_results["num_valid_ir_gap_valid_base_pairing_pairs"].append(
-                    len(separate_base_matching_ir_pairs)
-                )
-                experiment_results["ir_pair_index"].append(evaluated_pair_index)
-                experiment_results["ir_a"].append(ir_i)
-                experiment_results["ir_b"].append(ir_j)
-                experiment_results["ir_a_fe"].append(ir_a_fe)
-                experiment_results["ir_b_fe"].append(ir_b_fe)
-                experiment_results["ir_pair_fe_sum"].append(fe_sum)
-                experiment_results["ir_pair_fe_union"].append(fe_union)
-                experiment_results["irs_wholly_nested"].append(pair_type_bin_str[0])
-                experiment_results["irs_partially_nested"].append(pair_type_bin_str[1])
-                experiment_results["irs_disjoint"].append(pair_type_bin_str[2])
-                experiment_results["additivity_assumption_held"].append(assumption_held)
-                experiment_results["irs_form_valid_loop"].append(valid_loop_formed)
-
-                evaluated_pair_index += 1
+    print("IRs".center(60, "="))
+    for ir in found_irs:
+        print(ir)
 
     # Save results
     df = pd.DataFrame(experiment_results)
