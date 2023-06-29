d0e6324f3816415a477cd8f45a546bea95c785e2
DavidHurst <davidshrst@gmail.com>
2023-06-29 16:00:28 +0100

    Added analysis highlighting free energy values of IR pairs which form valid and invalid loops

--- scripts/experiment_1_results_analysis.py
+++ scripts/experiment_1_results_analysis.py
@@ -32,13 +32,32 @@ def plot_pair_analysis(pairs_df, pair_type):
             ],
             padding=8,
         )
-    plt.savefig(f"{DATA_DIR}/experiment_1/experiment_1_{pair_type}.png")
     plt.tight_layout()
+    plt.savefig(f"{DATA_DIR}/experiment_1/experiment_1_{pair_type}.png")
     plt.show()
 
 
 if __name__ == "__main__":
     results_df = pd.read_csv(f"{DATA_DIR}/experiment_1/experiment_1_results.csv")
+
+    # Show free energy value of pairs, highlight pairs that form valid loops and those that don't
+    form_valid_loop_highlighted_df = results_df.assign(
+        colour=['green' if val else 'red' for val in results_df.ir_pair_forms_valid_loop])
+    form_valid_loop_highlighted_df.ir_pair_fe_union = form_valid_loop_highlighted_df.ir_pair_fe_union.apply(
+        lambda x: x if x < 50_000 else 100)
+    bar_plot_ax = form_valid_loop_highlighted_df.plot.bar(
+        x="ir_pair_index",
+        y="ir_pair_fe_union",
+        color=form_valid_loop_highlighted_df.colour,
+        title=f"Free Energy Values of IR Pairs",
+        xticks=[],
+        xlabel="IR Pair",
+        ylabel="Pair Union Free Energy",
+        legend=False,  # ToDo: Add legend
+    )
+    plt.tight_layout()
+    plt.savefig(f"{DATA_DIR}/experiment_1/experiment_1_valid_loop_formed.png")
+    plt.show()
 
     # Show how the additivity assumption holds for pairs that form different structures e.g. wholly nested, disjoint...
     pairs_dfs = [
@@ -49,3 +68,4 @@ if __name__ == "__main__":
     pair_type_names = ["Disjoint", "Wholly Nested", "Partially Nested"]
     for pairs_df, pair_name in zip(pairs_dfs, pair_type_names):
         plot_pair_analysis(pairs_df, pair_name)
+
