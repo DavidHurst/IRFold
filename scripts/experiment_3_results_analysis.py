55aa5e24abb6dc68bfc82c82ab4405cac22efafd
DavidHurst <davidshrst@gmail.com>
2023-06-29 21:17:13 +0100

    Extended experiment 3 analysis to invalid IR triplets

--- scripts/experiment_3_results_analysis.py
+++ scripts/experiment_3_results_analysis.py
@@ -5,15 +5,11 @@ from pathlib import Path
 
 DATA_DIR = str(Path(__file__).parent.parent / "data")
 
-plt.rcParams["figure.figsize"] = (6, 4)
+plt.rcParams["figure.figsize"] = (14, 12)
 
-if __name__ == "__main__":
-    results_df = pd.read_csv(f"{DATA_DIR}/experiment_3/results.csv")
 
-    valid_loop_triplets_df = results_df[results_df["triplet_union_fe"] < 90_000]
-    valid_loop_triplets_df = valid_loop_triplets_df.reindex(
-        [i for i in range(len(valid_loop_triplets_df))]
-    )
+def plot_analysis(df, name):
+    df = df.reindex([i for i in range(len(df))])
 
     # Standardise values so that 0 is the true free energy (triplet_union_fe)
     for col in [
@@ -22,57 +18,47 @@ if __name__ == "__main__":
         "corrected_additive_fe_third_ir_pair",
         "corrected_additive_fe_all_pairs",
     ]:
-        valid_loop_triplets_df[col] = (
-            valid_loop_triplets_df[col] - valid_loop_triplets_df.triplet_union_fe
-        )
-    valid_loop_triplets_df.triplet_union_fe = (
-        valid_loop_triplets_df.triplet_union_fe
-        - valid_loop_triplets_df.triplet_union_fe
-    )
+        df[col] = df[col] - df.triplet_union_fe
+    df.triplet_union_fe = df.triplet_union_fe - df.triplet_union_fe
 
     fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
 
     # Plot triplets additive free energy with first IR pair corrected
     # ax1.scatter(valid_loop_triplets_df.index, valid_loop_triplets_df.triplet_union_fe)
-    ax1.bar(valid_loop_triplets_df.index,
-        valid_loop_triplets_df.index,
-        valid_loop_triplets_df.corrected_additive_fe_first_ir_pair,
-    )
-    ax1.set_title("Triplets' Free Energy w/ First IR Pair's Free Energy Corrected")
+    ax1.bar(df.index, df.corrected_additive_fe_first_ir_pair)
+    ax1.set_title("First IR Pair's Free Energy Corrected")
     ax1.set_xlabel("IR Triplet")
-    ax1.set_ylabel("Diff. from Triplet\'s True Free Energy")
+    ax1.set_ylabel("Free Energy - Difference from True")
 
     # Plot triplets additive free energy with second IR pair corrected
-    ax2.bar(
-        valid_loop_triplets_df.index,
-        valid_loop_triplets_df.corrected_additive_fe_second_ir_pair,
-    )
-    ax2.set_title("Triplets' Free Energy w/ Second IR Pair's Free Energy Corrected")
+    ax2.bar(df.index, df.corrected_additive_fe_second_ir_pair)
+    ax2.set_title("Second IR Pair's Free Energy Corrected")
     ax2.set_xlabel("IR Triplet")
-    ax2.set_ylabel("Diff. from Triplet\'s True Free Energy")
+    ax2.set_ylabel("Free Energy - Difference from True")
 
     # Plot triplets additive free energy with third IR pair corrected
-    ax3.bar(
-        valid_loop_triplets_df.index,
-        valid_loop_triplets_df.corrected_additive_fe_third_ir_pair,
-    )
-    ax3.set_title("Triplets' Free Energy w/ Third IR Pair's Free Energy Corrected")
+    ax3.bar(df.index, df.corrected_additive_fe_third_ir_pair)
+    ax3.set_title("Third IR Pair's Free Energy Corrected")
     ax3.set_xlabel("IR Triplet")
-    ax3.set_ylabel("Diff. from Triplet\'s True Free Energy")
+    ax3.set_ylabel("Free Energy - Difference from True")
 
     # Plot triplets additive free energy with all pairs corrected
-    ax4.bar(
-        valid_loop_triplets_df.index,
-        valid_loop_triplets_df.corrected_additive_fe_all_pairs,
-    )
-    ax4.set_title("Triplets' Free Energy w/ All IR Pairs' Free Energies Corrected")
+    ax4.bar(df.index, df.corrected_additive_fe_all_pairs)
+    ax4.set_title("All IR Pairs' Free Energies Corrected")
     ax4.set_xlabel("IR Triplet")
-    ax4.set_ylabel("Diff. from Triplet\'s True Free Energy")
+    ax4.set_ylabel("Free Energy - Difference from True")
 
     plt.tight_layout()
-    plt.legend()
+    # plt.suptitle(name)
+    plt.savefig(f"{DATA_DIR}/experiment_3/ir_triplets_fe_corrections_{name.lower()}.png")
     plt.show()
 
-    invalid_loop_triplets_df = results_df[
-        results_df["triplet_union_fe"] > 90_000
-    ].reindex()
+
+if __name__ == "__main__":
+    results_df = pd.read_csv(f"{DATA_DIR}/experiment_3/results.csv")
+
+    valid_loop_triplets_df = results_df[results_df["triplet_union_fe"] < 90_000]
+    invalid_loop_triplets_df = results_df[results_df["triplet_union_fe"] > 90_000]
+
+    plot_analysis(valid_loop_triplets_df, "Valid")
+    plot_analysis(invalid_loop_triplets_df, "Invalid")
