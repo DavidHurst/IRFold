cdc309147bad3d2e09d41fce8afb366d31951c33
DavidHurst <davidshrst@gmail.com>
2023-07-01 15:17:53 +0100

    Updated tests for IRs disjoint to use new fixture and added some tests

--- tests/test_irs_disjoint.py
+++ tests/test_irs_disjoint.py
@@ -1,15 +1,23 @@
-from irfold.util import ir_pair_disjoint
+from irfold.util import ir_pair_disjoint, irs_disjoint
 
 
-def test_irs_disjoint(entirely_disjoint_ir_pair):
-    ir_a = entirely_disjoint_ir_pair[0]
-    ir_b = entirely_disjoint_ir_pair[1]
+def test_method_ir_pair_disjoint_disjoint_ir_pair(disjoint_ir_pair):
+    ir_a = disjoint_ir_pair[0]
+    ir_b = disjoint_ir_pair[1]
 
     assert ir_pair_disjoint(ir_a, ir_b) == True
 
 
-def test_irs_not_disjoint(wholly_nested_ir_pair):
+def test_method_ir_pair_disjoint_not_disjoint_ir_pair(wholly_nested_ir_pair):
     ir_a = wholly_nested_ir_pair[0]
     ir_b = wholly_nested_ir_pair[1]
 
     assert ir_pair_disjoint(ir_a, ir_b) == False
+
+
+def test_method_irs_disjoint_disjoint_ir_pair(disjoint_ir_pair):
+    assert irs_disjoint(disjoint_ir_pair) == True
+
+
+def test_method_irs_disjoint_not_disjoint_ir_pair(wholly_nested_ir_pair):
+    assert irs_disjoint(wholly_nested_ir_pair) == False
