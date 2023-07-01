10cd39c3723e04a703b38ff63844ee4987701e54
DavidHurst <davidshrst@gmail.com>
2023-07-01 22:12:47 +0100

    Updated to use new fixtures

--- tests/test_method_get_solver.py
+++ tests/test_method_get_solver.py
@@ -2,42 +2,30 @@ from ortools.sat.python.cp_model import CpModel, IntVar
 
 import pytest
 
-from irfold import IRFoldBase, IRFoldVal1, IRFoldVal2, IRFoldCor2
+from irfold import (
+    IRFoldBase,
+    IRFoldVal1,
+    IRFoldVal2,
+    IRFoldCor2,
+    IRFoldCor3,
+    IRFoldCorX,
+)
 
 
-@pytest.mark.parametrize("irfold", [IRFoldBase, IRFoldVal1, IRFoldVal2, IRFoldCor2])
-def test_solver_generated_0_irs(data_dir, irfold, rna_seq_15_bases_0_irs):
-    seq = rna_seq_15_bases_0_irs[0]
-    seq_len = len(seq)
-
-    solver, variables = irfold.get_solver(
-        [],
-        seq_len,
-        seq,
+@pytest.mark.parametrize(
+    "ir_fold_variant",
+    [IRFoldBase, IRFoldVal1, IRFoldVal2, IRFoldCor2, IRFoldCor3, IRFoldCorX],
+)
+def test_not_none(
+    ir_fold_variant, all_irs, sequence, sequence_length, sequence_name, data_dir
+):
+    all_irs = list(all_irs)
+    solver, variables = ir_fold_variant.get_solver(
+        all_irs,
+        sequence_length,
+        sequence,
         data_dir,
-        "test_get_solver_seq",
-    )
-
-    assert solver is not None
-    assert variables is not None
-
-    assert isinstance(solver, CpModel)
-    assert isinstance(variables, list)
-
-    assert len(variables) == 0
-
-
-@pytest.mark.parametrize("irfold", [IRFoldBase, IRFoldVal1, IRFoldVal2, IRFoldCor2])
-def test_solver_generated_3_irs(data_dir, irfold, list_of_irs, rna_seq_15_bases_3_irs):
-    seq = rna_seq_15_bases_3_irs[0]
-    seq_len = len(seq)
-
-    solver, variables = irfold.get_solver(
-        list_of_irs,
-        seq_len,
-        seq,
-        data_dir,
-        "test_get_solver_seq",
+        sequence_name,
     )
 
     assert solver is not None
@@ -48,3 +36,83 @@ def test_solver_generated_3_irs(data_dir, irfold, list_of_irs, rna_seq_15_bases_
 
     for var in variables:
         assert isinstance(var, IntVar)
+
+
+@pytest.mark.parametrize(
+    "ir_fold_variant, variable_names",
+    [
+        (IRFoldBase, pytest.lazy_fixture("all_irs_names")),
+        (IRFoldVal1, pytest.lazy_fixture("ir_indicator_variables_names")),
+        (IRFoldVal2, pytest.lazy_fixture("ir_indicator_variables_names")),
+        (
+            IRFoldCor2,
+            pytest.lazy_fixture("ir_indicator_pair_corrector_variables_names"),
+        ),
+        (
+            IRFoldCor3,
+            pytest.lazy_fixture(
+                "ir_indicator_pair_corrector_triplet_corrector_variables_names"
+            ),
+        ),
+        # (IRFoldCorX, pytest.lazy_fixture("all_solver_variables_names")),  # Parametrise class first
+    ],
+)
+def test_number_of_variables_generated(
+    ir_fold_variant,
+    variable_names,
+    all_irs,
+    sequence,
+    sequence_length,
+    sequence_name,
+    data_dir,
+):
+    all_irs = list(all_irs)
+    _, variables = ir_fold_variant.get_solver(
+        all_irs,
+        sequence_length,
+        sequence,
+        data_dir,
+        sequence_name,
+    )
+
+    assert len(variables) == len(variable_names)
+
+
+@pytest.mark.parametrize(
+    "ir_fold_variant, variable_names",
+    [
+        (IRFoldBase, pytest.lazy_fixture("all_irs_names")),
+        (IRFoldVal1, pytest.lazy_fixture("ir_indicator_variables_names")),
+        (IRFoldVal2, pytest.lazy_fixture("ir_indicator_variables_names")),
+        (
+            IRFoldCor2,
+            pytest.lazy_fixture("ir_indicator_pair_corrector_variables_names"),
+        ),
+        (
+            IRFoldCor3,
+            pytest.lazy_fixture(
+                "ir_indicator_pair_corrector_triplet_corrector_variables_names"
+            ),
+        ),
+        # (IRFoldCorX, pytest.lazy_fixture("all_solver_variables_names")),  # Parametrise class first
+    ],
+)
+def test_variables_for_correct_variables_generated(
+    ir_fold_variant,
+    variable_names,
+    all_irs,
+    sequence,
+    sequence_length,
+    sequence_name,
+    data_dir,
+):
+    all_irs = list(all_irs)
+    _, variables = ir_fold_variant.get_solver(
+        all_irs,
+        sequence_length,
+        sequence,
+        data_dir,
+        sequence_name,
+    )
+
+    assert [v.Name() for v in variables] == variable_names
