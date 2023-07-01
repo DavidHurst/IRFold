8d6c9e4a7106fc0391bd12dbdc00fca964e12a2a
DavidHurst <davidshrst@gmail.com>
2023-07-01 15:34:20 +0100

    Updated to use new fixtures and added some tests

--- tests/test_irs_matching_same_bases.py
+++ tests/test_irs_matching_same_bases.py
@@ -1,15 +1,31 @@
-from irfold.util import ir_pair_match_same_bases
+from irfold.util import ir_pair_match_same_bases, irs_match_same_bases
 
 
-def test_irs_matching_same_bases(matching_same_bases_ir_pair):
-    ir_a = matching_same_bases_ir_pair[0]
-    ir_b = matching_same_bases_ir_pair[1]
+def test_method_ir_pair_match_same_bases_irs_matching_same_bases(
+    matching_the_same_bases_ir_pair,
+):
+    ir_a = matching_the_same_bases_ir_pair[0]
+    ir_b = matching_the_same_bases_ir_pair[1]
 
     assert ir_pair_match_same_bases(ir_a, ir_b) == True
 
 
-def test_irs_not_matching_same_bases(not_matching_same_bases_ir_pair):
-    ir_a = not_matching_same_bases_ir_pair[0]
-    ir_b = not_matching_same_bases_ir_pair[1]
+def test_method_ir_pair_match_same_bases_irs_not_matching_same_bases(
+    not_matching_the_same_bases_ir_pair,
+):
+    ir_a = not_matching_the_same_bases_ir_pair[0]
+    ir_b = not_matching_the_same_bases_ir_pair[1]
 
     assert ir_pair_match_same_bases(ir_a, ir_b) == False
+
+
+def test_method_irs_match_same_bases_irs_matching_same_bases(
+    matching_the_same_bases_ir_pair,
+):
+    assert irs_match_same_bases(matching_the_same_bases_ir_pair) == True
+
+
+def test_method_irs_match_same_bases_irs_not_matching_same_bases(
+    not_matching_the_same_bases_ir_pair,
+):
+    assert irs_match_same_bases(not_matching_the_same_bases_ir_pair) == False
