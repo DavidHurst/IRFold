04b1ff00e404cdf8883225e790aba7172cc3d1b8
DavidHurst <davidshrst@gmail.com>
2023-07-01 15:01:49 +0100

    Updated test to utilise new fixture

--- tests/test_ir_gap_size_validation.py
+++ tests/test_ir_gap_size_validation.py
@@ -1,9 +1,9 @@
 from irfold.util import ir_has_valid_gap_size
 
 
-def test_ir_gap_size_validation_invalid_ir(invalid_gap_size_ir):
+def test_ir_with_invalid_gap_size(invalid_gap_size_ir):
     assert ir_has_valid_gap_size(invalid_gap_size_ir) == False
 
 
-def test_ir_gap_size_validation_valid_ir(valid_gap_size_ir):
+def test_ir_with_valid_gap_size(valid_gap_size_ir):
     assert ir_has_valid_gap_size(valid_gap_size_ir) == True
