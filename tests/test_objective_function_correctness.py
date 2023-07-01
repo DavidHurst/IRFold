73bba60a61ee7dbc52e3d606e663b4f17119183c
DavidHurst <davidshrst@gmail.com>
2023-07-01 18:06:45 +0100

    Updated to use new fixtures

--- tests/test_objective_function_correctness.py
+++ tests/test_objective_function_correctness.py
@@ -1,36 +1,35 @@
-import pytest
-from irfold import IRFoldBase, IRFoldVal1, IRFoldVal2, IRFoldCor2, IRFoldCor3
+from irfold import (
+    IRFoldBase,
+    IRFoldVal1,
+    IRFoldVal2,
+    IRFoldCor2,
+    IRFoldCor3,
+)
 from irfold.util import calc_free_energy
 
 
-@pytest.mark.parametrize("irfold", [IRFoldBase, IRFoldVal1, IRFoldVal2, IRFoldCor2])
-def test_objective_function_does_not_exactly_match_true_free_energy(
-    irfold, find_irs_params, rna_seq_30_bases_19_irs, data_dir
-):
-    seq = rna_seq_30_bases_19_irs[0]
-    find_irs_params["sequence"] = seq
-    secondary_struct_pred, obj_fn_value = irfold.fold(**find_irs_params)
-
-    true_free_energy = calc_free_energy(
-        secondary_struct_pred, seq, data_dir, "test_irfold_obj_fn"
-    )
-
-    assert true_free_energy != obj_fn_value
-
-
 def test_objective_function_more_correct_with_increasing_model_version(
-    find_irs_params, rna_seq_30_bases_19_irs, data_dir
+    sequence,
+    sequence_length,
+    sequence_name,
+    data_dir,
+    all_irs,
 ):
-    seq = rna_seq_30_bases_19_irs[0]
-    find_irs_params["sequence"] = seq
-
     model_outputs = [
-        model.fold(**find_irs_params)
-        for model in [IRFoldBase, IRFoldVal1, IRFoldVal2, IRFoldCor2, IRFoldCor3]
+        ir_fold_variant.fold(
+            sequence, 2, sequence_length, sequence_name, out_dir=data_dir
+        )
+        for ir_fold_variant in [
+            IRFoldBase,
+            IRFoldVal1,
+            IRFoldVal2,
+            IRFoldCor2,
+            IRFoldCor3,
+        ]
     ]
 
     true_free_energies = [
-        calc_free_energy(struct_pred, seq, data_dir, "test_irfold_obj_fn")
+        calc_free_energy(struct_pred, sequence, data_dir, sequence_name)
         for struct_pred, _ in model_outputs
     ]
 
