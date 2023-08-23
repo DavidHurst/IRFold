1231ba69e6138ec63b979527d5bb34a9100eddda
DavidHurst <davidshrst@gmail.com>
2023-08-23 15:00:35 +0100

    Added analysis of pseudoknotted structures, bulge vs no bulge strucutres

--- experiment_scripts/experiment_5_results_analysis.py
+++ experiment_scripts/experiment_5_results_analysis.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 DATA_DIR = Path(__file__).parent.parent / "data"
 EXPERIMENT_5_DIR_PATH = DATA_DIR / "experiment_5"
+BP_RNA_1_M_ST_FILES_DIR = DATA_DIR / "bpRNA-1m" / "stFiles"
 
 if __name__ == "__main__":
     rnafold_res_df = pd.read_csv(
@@ -19,11 +20,10 @@ if __name__ == "__main__":
     irfold_val2_res_df = pd.read_csv(
         EXPERIMENT_5_DIR_PATH / "IRFoldVal2_benchmarking_results.csv"
     )
-    irfold_corx2_res_df = pd.read_csv(
-        EXPERIMENT_5_DIR_PATH / "IRFoldCorX2_benchmarking_results.csv"
-    )
 
-    print(f"Num. Samples: {len(irfold_corx2_res_df)}")
+    plt.rcParams["figure.figsize"] = (5, 3.5)
+
+    print(f"Num. Samples: {len(irfold_val2_res_df)}")
 
     # Print average F1 scores and running times for each model
     f1_scores = []
@@ -34,30 +34,195 @@ if __name__ == "__main__":
             rnastructure_res_df,
             ipknot_res_df,
             irfold_val2_res_df,
-            irfold_corx2_res_df,
         ],
-        ["RNAfold", "RNAstructure", "IPknot", "IRFoldVal2", "IRFoldCorX2"],
+        ["RNAfold", "RNAstructure", "IPknot", "IRFoldVal2"],
     ):
-
         f1_scores.append(res_df["f1"].mean())
         running_times.append(res_df["execution_wall_time_secs"].mean())
 
     print(
-        f"Model".ljust(15) + "Mean F1".center(9) + "Mean Running Time (secs)".center(21)
+        f"Model".ljust(15) + "Mean F1".center(9) + "Mean Running Time (secs)".center(31)
     )
-    print("-" * (15 + 9 + 21))
+    print("-" * (15 + 9 + 31))
     for model, f1, running_time in zip(
-        ["RNAfold", "RNAstructure", "IPknot", "IRFoldVal2", "IRFoldCorX2"],
+        ["RNAfold", "RNAstructure", "IPknot", "IRFoldVal2"],
         f1_scores,
         running_times,
     ):
         print(
             f"{model}".ljust(15)
             + f"{f1:.2f}".center(9)
-            + f"{running_time:.4f}".center(21)
+            + f"{running_time:.4f}".center(31)
         )
 
-    # Print and plot performance over sequence length intervals
+    # Get models' results DFs for pseudoknotted and non-pseudoknotted structures
+    sample_db_names = []
+    sample_db_nums = []
+    samples_contain_pk = []
+    samples_contain_bulge = []
+    for st_file_path in BP_RNA_1_M_ST_FILES_DIR.glob("*.st"):
+        sample_id = str(st_file_path).split("/")[-1].split(".")[0]
+        sample_db_name = sample_id.split("_")[1]
+        sample_db_num = sample_id.split("_")[-1]
+
+        contains_pk = False
+        contains_bulge = False
+
+        with open(st_file_path, "r") as st_file:
+            lines = st_file.readlines()
+            for line in lines[7:]:
+                if "PK" in line:
+                    contains_pk = True
+                if "B" in line:
+                    contains_bulge = True
+
+        sample_db_names.append(sample_db_name)
+        sample_db_nums.append(int(sample_db_num))
+        samples_contain_pk.append(contains_pk)
+        samples_contain_bulge.append(contains_bulge)
+    samples_pk_status_df = pd.DataFrame(
+        {
+            "database_name": sample_db_names,
+            "sequence_database_number": pd.Series(sample_db_nums, dtype="int64"),
+            "contains_pk": samples_contain_pk,
+            "contains_bulge": samples_contain_bulge,
+        }
+    )
+
+    # Inner join results DFs with pk_status=True DF to get results of pseudoknotted sequences
+    pk_samples = samples_pk_status_df[samples_pk_status_df.contains_pk == True]
+    rnafold_res_df_pk = pd.merge(
+        rnafold_res_df, pk_samples, on=["database_name", "sequence_database_number"]
+    )
+    rnastructure_res_df_pk = pd.merge(
+        rnastructure_res_df,
+        pk_samples,
+        on=["database_name", "sequence_database_number"],
+    )
+    ipknot_res_df_pk = pd.merge(
+        ipknot_res_df, pk_samples, on=["database_name", "sequence_database_number"]
+    )
+    irfold_val2_res_df_pk = pd.merge(
+        irfold_val2_res_df, pk_samples, on=["database_name", "sequence_database_number"]
+    )
+
+    print(rnastructure_res_df_pk.sample(3))
+
+    # Inner join results DFs with pk_status=False DF to get results of pseudoknotted sequences
+    no_pk_samples = samples_pk_status_df[samples_pk_status_df.contains_pk == False]
+    rnafold_res_df_no_pk = pd.merge(
+        rnafold_res_df, no_pk_samples, on=["database_name", "sequence_database_number"]
+    )
+    rnastructure_res_df_no_pk = pd.merge(
+        rnastructure_res_df,
+        no_pk_samples,
+        on=["database_name", "sequence_database_number"],
+    )
+    ipknot_res_df_no_pk = pd.merge(
+        ipknot_res_df, no_pk_samples, on=["database_name", "sequence_database_number"]
+    )
+    irfold_val2_res_df_no_pk = pd.merge(
+        irfold_val2_res_df,
+        no_pk_samples,
+        on=["database_name", "sequence_database_number"],
+    )
+
+    print(rnastructure_res_df_no_pk.sample(3))
+
+    markers = {"RNAfold": "o", "RNAstructure": "+", "IPknot": "s", "IRFoldVal2": "^"}
+
+    # Plot PPV against Sens for non-pseudoknotted structures
+    ppv_scores = []
+    sens_scores = []
+    for res_df, df_name in zip(
+        [
+            rnafold_res_df_no_pk,
+            rnastructure_res_df_no_pk,
+            ipknot_res_df_no_pk,
+            irfold_val2_res_df_no_pk,
+        ],
+        ["RNAfold", "RNAstructure", "IPknot", "IRFoldVal2"],
+    ):
+        ppv_scores.append(res_df["sensitivity"].mean())
+        sens_scores.append(res_df["ppv"].mean())
+
+    for model_name, mean_ppv, mean_sens in zip(
+        ["RNAfold", "RNAstructure", "IPknot", "IRFoldVal2"], ppv_scores, sens_scores
+    ):
+        plt.scatter(
+            mean_ppv, mean_sens, label=model_name, marker=markers[model_name], s=50
+        )
+
+    plt.legend()
+    plt.xlabel("Positive Predictive Value")
+    plt.ylabel("Sensitivity")
+    plt.tight_layout()
+    plt.savefig(f"{EXPERIMENT_5_DIR_PATH}/ppv_vs_sens_no_pk.png")
+    plt.show()
+
+    # Plot PPV against Sens for pseudoknotted structures
+    ppv_scores = []
+    sens_scores = []
+    for res_df, df_name in zip(
+        [
+            rnafold_res_df_pk,
+            rnastructure_res_df_pk,
+            ipknot_res_df_pk,
+            irfold_val2_res_df_pk,
+        ],
+        ["RNAfold", "RNAstructure", "IPknot", "IRFoldVal2"],
+    ):
+        ppv_scores.append(res_df["sensitivity"].mean())
+        sens_scores.append(res_df["ppv"].mean())
+
+    for model_name, mean_ppv, mean_sens in zip(
+        ["RNAfold", "RNAstructure", "IPknot", "IRFoldVal2"], ppv_scores, sens_scores
+    ):
+        plt.scatter(
+            mean_ppv, mean_sens, label=model_name, marker=markers[model_name], s=50
+        )
+
+    plt.legend()
+    plt.xlabel("Positive Predictive Value")
+    plt.ylabel("Sensitivity")
+    plt.tight_layout()
+    plt.savefig(f"{EXPERIMENT_5_DIR_PATH}/ppv_vs_sens_pk.png")
+    plt.show()
+
+    # Plot PPV vs sens for IRFold on samples with and without bulge loops
+    no_bulge_samples = samples_pk_status_df[
+        samples_pk_status_df.contains_bulge == False
+    ]
+    bulge_samples = samples_pk_status_df[samples_pk_status_df.contains_bulge == True]
+    irfold_val2_res_df_no_bulge = pd.merge(
+        irfold_val2_res_df,
+        no_bulge_samples,
+        on=["database_name", "sequence_database_number"],
+    )
+    irfold_val2_res_df_bulge = pd.merge(
+        irfold_val2_res_df,
+        bulge_samples,
+        on=["database_name", "sequence_database_number"],
+    )
+
+    bulge_sens = irfold_val2_res_df_bulge["sensitivity"].mean()
+    bulge_ppv = irfold_val2_res_df_bulge["ppv"].mean()
+    no_bulge_sens = irfold_val2_res_df_no_bulge["sensitivity"].mean()
+    no_bulge_ppv = irfold_val2_res_df_no_bulge["ppv"].mean()
+
+    print(f'No bulge mean F1: {irfold_val2_res_df_no_bulge["f1"].mean():.4f}')
+    print(f'Bulge mean F1: {irfold_val2_res_df_bulge["f1"].mean():.4f}')
+
+    plt.scatter(bulge_ppv, bulge_sens, label="Bulge Loops", marker="s", s=50)
+    plt.scatter(no_bulge_ppv, no_bulge_sens, label="No Bulge Loops", marker="^", s=50)
+    plt.legend()
+    plt.xlabel("Positive Predictive Value")
+    plt.ylabel("Sensitivity")
+    plt.tight_layout()
+    plt.savefig(f"{EXPERIMENT_5_DIR_PATH}/ppv_vs_sens_bulge.png")
+    plt.show()
+
+    # Plot mean F1 as sequence length increases
     seq_interval_sz = 10
     avg_df = rnafold_res_df.groupby("sequence_length").mean(numeric_only=True)
     seq_intervals = [
@@ -87,28 +252,11 @@ if __name__ == "__main__":
     col_width = 15
     n_intervals = len(seq_intervals)
 
-    print(
-        "".join(
-            ["              ".ljust(col_width)]
-            + [f"F1 Mean".center(col_width * n_intervals)]
-        )
-    )
-    print("-" * (col_width + (col_width * n_intervals)))
-
-    print(
-        "".join(
-            ["Model\Interval".ljust(col_width)]
-            + [f"{i[0]}-{i[1]}".center(col_width) for i in seq_intervals]
-        )
-    )
-    print("-" * (col_width * (n_intervals + 1)))
-
     model_seq_len_interval_f1_means = {
         "RNAfold": [],
         "RNAstructure": [],
         "IPknot": [],
         "IRFoldVal2": [],
-        "IRFoldCorX2": [],
     }
     for res_df, df_name in zip(
         [
@@ -116,9 +264,8 @@ if __name__ == "__main__":
             rnastructure_res_df,
             ipknot_res_df,
             irfold_val2_res_df,
-            irfold_corx2_res_df,
         ],
-        ["RNAfold", "RNAstructure", "IPknot", "IRFoldVal2", "IRFoldCorX2"],
+        ["RNAfold", "RNAstructure", "IPknot", "IRFoldVal2"],
     ):
         avg_df = res_df.groupby("sequence_length").mean(numeric_only=True)
 
@@ -129,14 +276,12 @@ if __name__ == "__main__":
 
         model_seq_len_interval_f1_means[df_name] = seq_interval_means
 
-        print(
-            "".join(
-                [df_name.ljust(col_width)]
-                + [f" {f1:.2f} ".center(col_width) for f1 in seq_interval_means]
-            )
-        )
-
-    plt.rcParams["figure.figsize"] = (8, 6)
+        # print(
+        #     "".join(
+        #         [df_name.ljust(col_width)]
+        #         + [f" {f1:.2f} ".center(col_width) for f1 in seq_interval_means]
+        #     )
+        # )
 
     for model_name, f1_inverval_means in model_seq_len_interval_f1_means.items():
         plt.plot(
@@ -145,12 +290,12 @@ if __name__ == "__main__":
             label=model_name,
         )
 
-    plt.bar(
-        [i for i in range(n_intervals)],
-        normalised_interval_counts,
-        label="Sample Dist.",
-        alpha=0.3,
-    )
+    # plt.bar(
+    #     [i for i in range(n_intervals)],
+    #     normalised_interval_counts,
+    #     label="Sample Dist.",
+    #     alpha=0.3,
+    # )
 
     plt.xticks(
         [i for i in range(n_intervals)],
