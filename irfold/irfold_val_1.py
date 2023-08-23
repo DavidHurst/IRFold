c6ddfa84bed7c7f25aef970293b73c9692cd7857
DavidHurst <davidshrst@gmail.com>
2023-08-23 14:54:57 +0100

    Changed CP model function to return correct model for IRFold variant

--- irfold/irfold_val_1.py
+++ irfold/irfold_val_1.py
@@ -1,6 +1,5 @@
 __all__ = ["IRFoldVal1"]
 
-import itertools
 import re
 
 from ortools.sat.python.cp_model import CpModel, IntVar, LinearExpr
@@ -11,7 +10,6 @@ from irfold.util import (
     IR,
     irs_to_dot_bracket,
     calc_free_energy,
-    irs_incompatible,
 )
 
 
@@ -19,7 +17,7 @@ class IRFoldVal1(IRFoldBase):
     """Extends base IRFold model by validating found IRs in pairs before passing them to the solver."""
 
     @staticmethod
-    def _get_solver(
+    def _get_cp_model(
         ir_list: List[IR],
         seq_len: int,
         sequence: str,
@@ -31,51 +29,20 @@ class IRFoldVal1(IRFoldBase):
         n_irs: int = len(ir_list)
 
         # Create binary indicator variables for IRs
-        invalid_gap_ir_idxs: List[int] = [
+        invalid_gap_sz_ir_idxs: List[int] = [
             i for i in range(n_irs) if not ir_has_valid_gap_size(ir_list[i])
         ]
         ir_indicator_variables = [
             model.NewIntVar(0, 1, f"ir_{i}")
             for i in range(n_irs)
-            if i not in invalid_gap_ir_idxs
+            if i not in invalid_gap_sz_ir_idxs
         ]
 
         # If 1 or fewer variables, trivial or impossible optimisation problem, will be trivially handled by solver
         if len(ir_indicator_variables) <= 1:
             return model, ir_indicator_variables
 
-        unique_possible_idx_pairs: List[Tuple[int, int]] = list(
-            itertools.combinations([i for i in range(n_irs)], 2)
-        )
-        valid_idx_pairs: List[Tuple[int, int]] = [
-            pair
-            for pair in unique_possible_idx_pairs
-            if pair[0] not in invalid_gap_ir_idxs and pair[1] not in invalid_gap_ir_idxs
-        ]
-        valid_ir_pairs: List[Tuple[IR, IR]] = [
-            (ir_list[i], ir_list[j]) for i, j in valid_idx_pairs
-        ]
-        incompatible_ir_pair_idxs: List[Tuple[int, int]] = [
-            idx_pair
-            for ir_pair, idx_pair in zip(valid_ir_pairs, valid_idx_pairs)
-            if irs_incompatible([ir_pair[0], ir_pair[1]])
-        ]
-
-        # Add XOR between IRs that are incompatible
-        for ir_a_idx, ir_b_idx in incompatible_ir_pair_idxs:
-            # Search required as some IR variables might not have had variables created as they were invalid so
-            # list ordering of variables cannot be relied upon
-            ir_a_var: IntVar = [
-                var for var in ir_indicator_variables if str(ir_a_idx) in var.Name()
-            ][0]
-            ir_b_var: IntVar = [
-                var for var in ir_indicator_variables if str(ir_b_idx) in var.Name()
-            ][0]
-
-            model.Add(ir_a_var + ir_b_var <= 1)
-
         # All constraints and the objective must have integer coefficients for CP-SAT solver
-
         # Obtain free energies of the IRs that are valid, they comprise the coefficients for ir vars
         variable_coefficients: List[int] = []
         for var in ir_indicator_variables:
