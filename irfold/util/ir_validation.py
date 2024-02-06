c8cd9d6c8c727a6f80e1ede2f1d850b888311b9e
DavidHurst <davidshrst@gmail.com>
2024-02-06 20:06:18 +0000

    Renamed checks to refer to IR relative positioning

--- irfold/util/ir_validation.py
+++ irfold/util/ir_validation.py
@@ -11,7 +11,14 @@ def ir_has_valid_gap_size(ir: IR):
     return right_strand_start_idx - left_strand_end_idx - 1 >= 3
 
 
-def ir_pair_match_same_bases(ir_a: IR, ir_b: IR) -> bool:
+def irs_incompatible(ir_list: List[IR]) -> bool:
+    return irs_co_located(ir_list) or not irs_valid_relative_pos(ir_list)
+
+
+# =================== Co-location checks ===================
+
+
+def ir_pair_co_located(ir_a: IR, ir_b: IR) -> bool:
     ir_a_left_strand, ir_a_right_strand = ir_a[0], ir_a[1]
 
     # Accumulate paired base indices of ir_a
@@ -29,24 +36,20 @@ def ir_pair_match_same_bases(ir_a: IR, ir_b: IR) -> bool:
     return any([ir_b_bases in paired_base_idxs_a for ir_b_bases in paired_base_idxs_b])
 
 
-def irs_match_same_bases(ir_list: List[IR]) -> bool:
+def irs_co_located(ir_list: List[IR]) -> bool:
     """Returns true if any IRs in the given list match the same bases"""
     return any(
         [
-            ir_pair_match_same_bases(ir_a, ir_b)
+            ir_pair_co_located(ir_a, ir_b)
             for ir_a, ir_b in list(itertools.combinations(ir_list, 2))
         ]
     )
 
 
-def irs_incompatible(ir_list: List[IR]) -> bool:
-    return irs_match_same_bases(ir_list) or not irs_form_valid_loop(ir_list)
+# =================== Not-nested checks ===================
 
 
-# =================== Disjoint checks ===================
-
-
-def ir_pair_disjoint(ir_a: IR, ir_b: IR) -> bool:
+def ir_pair_not_nested(ir_a: IR, ir_b: IR) -> bool:
     # ir_a comes entirely before ir_b
     ir_a_strictly_before_ir_b = ir_a[1][1] < ir_b[0][0]
 
@@ -60,7 +63,7 @@ def ir_a_precedes_ir_b(ir_a: IR, ir_b: IR) -> bool:
     return ir_a[1][1] < ir_b[0][0]  # ir_a comes entirely before ir_b
 
 
-def irs_disjoint(ir_list: List[IR]) -> bool:
+def irs_not_nested(ir_list: List[IR]) -> bool:
     # Sort IRs by left strand start
     sorted_irs: List[IR] = sorted(ir_list, key=lambda ir: ir[0][0])
 
@@ -132,10 +135,11 @@ def irs_wholly_nested(ir_list: List[IR]) -> bool:
     return True
 
 
-# =================== Bases in intersection(s) checks ===================
+# =================== Partial nesting checks ===================
 
 
-def ir_pair_intersection_has_valid_base_count(ir_a: IR, ir_b: IR) -> bool:
+def ir_pair_partially_nested(ir_a: IR, ir_b: IR) -> bool:
+    """Returns true if ir_a is partially nested with ir_b i.e. if ir_a and ir_b form a pseudoknot with each other."""
     ir_a_left_strand: Tuple[int, int] = ir_a[0]
     ir_b_left_strand: Tuple[int, int] = ir_b[0]
 
@@ -156,25 +160,31 @@ def ir_pair_intersection_has_valid_base_count(ir_a: IR, ir_b: IR) -> bool:
     return num_bases_inbetween_latest_left_and_earliest_right_bases >= 3
 
 
-# =================== Valid loop checks ===================
-
-
-def ir_pair_forms_valid_loop(ir_a: IR, ir_b: IR) -> bool:
-    return (
-        ir_pair_wholly_nested(ir_a, ir_b)
-        or ir_pair_disjoint(ir_a, ir_b)
-        or ir_pair_intersection_has_valid_base_count(ir_a, ir_b)
+def irs_partially_nested(ir_list: List[IR]) -> bool:
+    """Returns true if any of the IRs are partially nested i.e. if any of the
+    IRs form pseudoknots with each other."""
+    return all(
+        [
+            ir_pair_partially_nested(ir_a, ir_b)
+            for ir_a, ir_b in list(itertools.combinations(ir_list, 2))
+        ]
     )
 
 
-def irs_form_valid_loop(ir_list: List[IR]) -> bool:
+# =================== Combined relative positioning checks ===================
+
+
+def ir_pair_valid_relative_pos(ir_a: IR, ir_b: IR) -> bool:
     return (
-        irs_disjoint(ir_list)
+        ir_pair_wholly_nested(ir_a, ir_b)
+        or ir_pair_not_nested(ir_a, ir_b)
+        or ir_pair_partially_nested(ir_a, ir_b)
+    )
+
+
+def irs_valid_relative_pos(ir_list: List[IR]) -> bool:
+    return (
+        irs_not_nested(ir_list)
         or irs_wholly_nested(ir_list)
-        or all(
-            [
-                ir_pair_intersection_has_valid_base_count(ir_a, ir_b)
-                for ir_a, ir_b in list(itertools.combinations(ir_list, 2))
-            ]
-        )
+        or irs_partially_nested(ir_list)
     )
