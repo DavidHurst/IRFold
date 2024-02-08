2e4c6b0ec879b91895fdcc42603c97e95b76a9b1
DavidHurst <davidshrst@gmail.com>
2024-02-08 20:37:25 +0000

    Removed unused IR checks and IR checks accepting lists of IRs

--- irfold/util/ir_validation.py
+++ irfold/util/ir_validation.py
@@ -1,5 +1,4 @@
-import itertools
-from typing import Tuple, List
+from typing import Tuple
 
 from .helper_functions import IR
 
@@ -9,9 +8,6 @@ def ir_has_valid_gap_size(ir: IR):
     right_strand_start_idx: int = ir[1][0]
 
     return right_strand_start_idx - left_strand_end_idx - 1 >= 3
-
-
-# =================== Co-location checks ===================
 
 
 def ir_pair_co_located(ir_a: IR, ir_b: IR) -> bool:
@@ -32,19 +28,6 @@ def ir_pair_co_located(ir_a: IR, ir_b: IR) -> bool:
     return any([ir_b_bases in paired_base_idxs_a for ir_b_bases in paired_base_idxs_b])
 
 
-def irs_co_located(ir_list: List[IR]) -> bool:
-    """Returns true if any IRs in the given list match the same bases"""
-    return any(
-        [
-            ir_pair_co_located(ir_a, ir_b)
-            for ir_a, ir_b in list(itertools.combinations(ir_list, 2))
-        ]
-    )
-
-
-# =================== Not-nested checks ===================
-
-
 def ir_pair_not_nested(ir_a: IR, ir_b: IR) -> bool:
     # ir_a comes entirely before ir_b
     ir_a_strictly_before_ir_b = ir_a[1][1] < ir_b[0][0]
@@ -53,29 +36,6 @@ def ir_pair_not_nested(ir_a: IR, ir_b: IR) -> bool:
     ir_b_strictly_before_ir_a = ir_b[1][1] < ir_a[0][0]
 
     return ir_a_strictly_before_ir_b or ir_b_strictly_before_ir_a
-
-
-def ir_a_precedes_ir_b(ir_a: IR, ir_b: IR) -> bool:
-    return ir_a[1][1] < ir_b[0][0]  # ir_a comes entirely before ir_b
-
-
-def irs_not_nested(ir_list: List[IR]) -> bool:
-    # Sort IRs by left strand start
-    sorted_irs: List[IR] = sorted(ir_list, key=lambda ir: ir[0][0])
-
-    # Check that first IR starts and ends before second IR which starts and ends before third IR...
-    # ToDo: List comprehension-ify this
-    for i in range(len(ir_list) - 1):
-        preceding_ir: IR = sorted_irs[i]
-        succeeding_ir: IR = sorted_irs[i + 1]
-
-        if not ir_a_precedes_ir_b(preceding_ir, succeeding_ir):
-            return False
-
-    return True
-
-
-# =================== Wholly nested checks ===================
 
 
 def ir_pair_wholly_nested(ir_a: IR, ir_b: IR) -> bool:
@@ -100,38 +60,6 @@ def ir_pair_wholly_nested(ir_a: IR, ir_b: IR) -> bool:
 
     return ir_b_in_ir_a or ir_a_in_ir_b
 
-
-def ir_a_subsumes_ir_b(ir_a: IR, ir_b: IR) -> bool:
-    ir_a_left_strand_end_idx: int = ir_a[0][1]
-    ir_a_right_strand_start_idx: int = ir_a[1][0]
-
-    ir_b_left_strand_start_idx: int = ir_b[0][0]
-    ir_b_right_strand_end_idx: int = ir_b[1][1]
-
-    return (
-        ir_a_left_strand_end_idx < ir_b_left_strand_start_idx
-        and ir_a_right_strand_start_idx > ir_b_right_strand_end_idx
-    )
-
-
-def irs_wholly_nested(ir_list: List[IR]) -> bool:
-    """Checks for one IR at each nesting level only i.e. first IR subsumes second which subsumes third IR etc."""
-    # Sort IRs by left strand start
-    sorted_irs: List[IR] = sorted(ir_list, key=lambda ir: ir[0][0])
-
-    # Check that first IR subsumes second IR which subsumes third IR which subsumes fourth IR...
-    # ToDo: List comprehension-ify this
-    for i in range(len(ir_list) - 1):
-        preceding_ir: IR = sorted_irs[i]
-        succeeding_ir: IR = sorted_irs[i + 1]
-
-        if not ir_a_subsumes_ir_b(preceding_ir, succeeding_ir):
-            return False
-
-    return True
-
-
-# =================== Partial nesting checks ===================
 
 def ir_pair_partially_nested(ir_a: IR, ir_b: IR) -> bool:
     """Returns true if ir_a is partially nested with ir_b."""
@@ -184,20 +112,6 @@ def ir_pair_partially_nested(ir_a: IR, ir_b: IR) -> bool:
         return True
 
     return False
-
-
-def irs_partially_nested(ir_list: List[IR]) -> bool:
-    """Returns true if any of the IRs are partially nested i.e. if any of the
-    IRs form pseudoknots with each other."""
-    return any(
-        [
-            ir_pair_partially_nested(ir_a, ir_b)
-            for ir_a, ir_b in list(itertools.combinations(ir_list, 2))
-        ]
-    )
-
-
-# =================== Combined relative positioning checks ===================
 
 
 def ir_pair_invalid_relative_pos(ir_a: IR, ir_b: IR) -> bool:
