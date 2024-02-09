a893f10d3484e32fe295c5bbadcb91baed2a4cdd
DavidHurst <davidshrst@gmail.com>
2024-02-09 19:39:57 +0000

    Updated demo file

--- demo.py
+++ demo.py
@@ -1,14 +1,16 @@
-from irfold import IRFoldVal2
-
+from irfold import IRfold
+from irfold.util.helper_functions import calc_free_energy
 
 if __name__ == "__main__":
-    seq_len = 50
-    seq = "GGGUGGCGCCAAUGUGAGACCCCGUACAGUCGCUUGAACAUGAGGUCGAA"
 
-    folded_sequence, obj_fn_value = IRFoldVal2.fold(seq)
+    seq = "UGAUGACUUAUGCUUAACCAAAGCACGGCA"
 
-    print(f"Sequence: {seq}")
-    print(f"Sequence length: {seq_len}")
+    folded_sequence, obj_fn_value = IRfold.fold(seq, show_prog=True)
 
-    print(f"Folded sequence (Dot Bracket) : {folded_sequence}")
-    print(f"Objective Function Final Value: {obj_fn_value}")
+    print(f"Sequence length               : {len(seq)}")
+    print(f"Sequence                      : {seq}")
+    print(f"Folded sequence (Dot bracket) : {folded_sequence}")
+    print(f"Objective function final value: {obj_fn_value:.4f}")
+    print(
+        f"Pred. free energy (ViennaRNA) : {calc_free_energy(folded_sequence, seq, './'):.4f}"
+    )
